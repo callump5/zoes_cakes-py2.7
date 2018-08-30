8b0a9ef76e8412083a1c31c5af95baba81a7eddd
Callum <clpullinger@gmail.com>
2018-08-30 23:02:40 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn zoe_cakes.wsgi :app
---
+++ new_settings/staging.py
@@ -0,0 +1,25 @@
+from base import *
+import dj_database_url
+
+DEBUG = False
+
+DATABASES = {
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+}
+
+# Log DEBUG information to the console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
