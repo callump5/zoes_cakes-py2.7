ae76d99fbc649c43bbe11587643ed449ce495df4
Callum <clpullinger@gmail.com>
2018-09-16 21:58:55 +0100

    push to heroku

--- new_settings/base.py
+++ new_settings/base.py
@@ -135,4 +135,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_SECURE_URLS = False       # use http instead of https
 AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related query parameters for requests
 
-AWS_STORAGE_BUCKET_NAME = 'zoes-cakes'
+AWS_STORAGE_BUCKET_NAME = 'zoes-cakes-irl'
