506acfa967c5f57ffd2c7cfad2c41105a0d3667e
Callum <clpullinger@gmail.com>
2018-09-16 21:08:53 +0100

    push to heroku

--- home/urls.py
+++ home/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-import views
+
+from .views import get_items
 
 urlpatterns = [
-
-    url(r'^category/(?P<slug>[-\w]+)/$', views.get_items, name='category'),
+    url(r'^category/(?P<slug>[-\w]+)/$', get_items, name='category')
 ]
-
--- home/views.py
+++ home/views.py
@@ -3,16 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Category, Item
-from blog.models import BlogPost
 # Create your views here.
 
 def get_home(request):
     category = Category.objects.all()
-    posts = BlogPost.objects.all()[0:2]
-    return render(request, 'home/home.html', {'category': category,
-                                              'posts': posts})
-
+    return render(request, 'home/home.html', {'category': category})
 
 def get_items(request, slug):
-    items = Item.objects.filter(category__name__startswith=slug)
-    return render(request, 'categorys/items.html', {'items': items})
+    items = Item.objects.filter(category__name__contains=slug)
+    return render(request, 'home/category_items.html', {'items': items,
+                                                        'slug': slug})
