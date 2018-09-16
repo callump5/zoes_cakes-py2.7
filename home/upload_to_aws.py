19ddde18a7a8d1645ea6f4947b591a2b6b3ee585
Callum <clpullinger@gmail.com>
2018-09-16 21:45:47 +0100

    push to heroku

--- home/upload_to_aws.py
+++ home/upload_to_aws.py
@@ -3,7 +3,7 @@ from django.utils.timezone import now
 
 def upload_img(instance, filename):
     filename_base, filename_ext = os.path.splitext(filename)
-    return 'staff/%s%s' % (
+    return 'zoes-cakes/%s%s' % (
         now().strftime("%Y%m%d%H%M%S"),
         filename_ext.lower(),
     )
